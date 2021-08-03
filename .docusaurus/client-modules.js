98e50ee18d4a8605b40c160fc2081d8e01736c2b
fu31 <torahikoshinoda0101@gmail.com>
2021-08-03 21:33:57 +0900

    deleted build and .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/stevekimura/project/hokusai-api-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/stevekimura/project/hokusai-api-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/stevekimura/project/hokusai-api-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/stevekimura/project/hokusai-api-docs/src/css/custom.css"),
-];
